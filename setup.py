0fa47eb0b3e460f655ef2e83c72263938017562e
ShafaiJMI <imamshafai02@gmail.com>
2022-09-09 13:27:44 +0530

    changes in setup

--- setup.py
+++ setup.py
@@ -19,20 +19,28 @@ def proximity(trg,echo):
     distance = (timeElapsed * 34300)/2
     return int(distance)
 
-def lid():
-    kit = ServoKit(channels=16)
-    servo1 = kit.servo[0]
-    servo1.angle = 90
-    #kit.continious_servo[1].throttle = 1
-    #time.sleep(2)
-    #servo1.angle = 90
-    #kit.conitnious_servo[1].throttle = -1
-    #time.sleep(4)
-    servo1.angle = 45
-    #time.sleep(0.5)
-    #servo1.angle = 0
-    #kit.continious_servo[1].throttle = 0
-    servo1.angle = 0
+class Lid:
+    def __init__(self):
+        self.kit = ServoKit(channels=16)
+        self.servo1 = kit.servo[0]
+    def demo(self):
+        self.servo1.angle = 45
+        #kit.continious_servo[1].throttle = 1
+        time.sleep(1)
+        self.servo1.angle = 90
+        #kit.conitnious_servo[1].throttle = -1
+        time.sleep(4)
+        self.servo1.angle = 45
+        time.sleep(0.5)
+        #servo1.angle = 0
+        #kit.continious_servo[1].throttle = 0
+        self.servo1.angle = 0
+    def open(self):
+        self.servo1.angle = 90
+        time.sleep(4)
+        self.servo1.angle = 45
+        time.sleep(0.5)
+        self.servo1.angle = 90
 
 if __name__ == '__main__':
     try:
@@ -41,7 +49,7 @@ if __name__ == '__main__':
             dis = proximity(18,24)
             #print(dis)
             if dis <= 30:
-                lid()
+                Lid.open()
             time.sleep(1)
     except KeyboardInterrupt:
         print("Setup Stopped")
